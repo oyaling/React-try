2ab3ee63d8aca4113f5f2cd88662a8b8bcd50d3f
oyaling <yaling@venty.com.tw>
2018-11-27 16:43:54 +0800

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'React-try',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learn to design and code React apps</h1>
+    <p>complete courses about the best tools and design systems. Prototype and build apps with React and swift.</p>
+    <Link to="/page-2/">Watch the video</Link>
+  </div>
+)
+
+export default IndexPage
